668342679d1526997bd720137ff633672b0c1bd8
Afaq Khan <101743351+afaq-khan2000@users.noreply.github.com>
2024-09-12 01:01:24 +0500

    initial commit

---
+++ src/assets/images/index.js
@@ -0,0 +1,3 @@
+export { default as bannerBg } from './bg.avif';
+export { default as NavLogo } from './nav-logo.svg';
+export { default as WorkProcess } from './my-work.avif';
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { Banner, WorkMethod } from "../components/home";
+
+function Home() {
+
+  return (
+    <>
+      <Banner />
+      <WorkMethod />
+    </>
+  );
+}
+
+export default Home;
